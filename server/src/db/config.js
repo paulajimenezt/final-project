3a23ff758e5c847e8eb8edf8157d10291ed8ace2
Paula Jiménez Tamayo <paula.jimenez@actionblack.co>
2024-06-02 16:38:42 -0500

    adding environment variables for database

--- server/src/db/config.js
+++ server/src/db/config.js
@@ -1,7 +1,10 @@
+require("dotenv").config();
+
 module.exports = {
-  username: "root",
-  password: "root",
-  database: "health-horse-db",
-  host: "localhost",
-  dialect: "postgres",
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  dialect: process.env.DB_DIALECT,
 };
